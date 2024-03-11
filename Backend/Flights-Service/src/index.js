bd801202b41929b5191cec574a5813ba02c4db04
surjeetvermaiitm <surjeetverma2017@gmail.com>
2024-03-12 00:21:54 +0530

    airplane entity with crud functionality implemented

---
+++ Backend/Flights-Service/src/index.js
@@ -0,0 +1,17 @@
+const { ServerConfig, Logger } = require("./config"); 
+const express = require("express");
+const apiRoutes = require("./routes");
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use("/api", apiRoutes); 
+
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Server is up and running on PORT: ${ServerConfig.PORT}`);
+  Logger.info("Successfully started the Server!", "root");
+});
+
+
